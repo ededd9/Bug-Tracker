1cd6041e821ac8627740ed78b99094f13e3a3845
Edward Hurtado <edhur9@gmail.com>
2022-03-05 23:01:06 -0500

    made root request async

--- server.js
+++ server.js
@@ -2,28 +2,26 @@ const express = require('express');
 const mainpageRouter = require('./routes/mainpage');
 const addTicketRouter = require('./routes/addTicketRouter');
 const mongooseDB = require('./database/ticketsData');
-const Ticket = require('./models/tickets');
-const methodOverride = require('method-override');
-const app = express();
+const bodyParser = require('body-parser');
 
-app.use(express.static('public'));
+const methodOverride = require('method-override');
+const Ticket = require('./models/tickets');
+const app = express();
+//register view engine
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/mainpage', mainpageRouter);
 app.use('/addTicketScreen', addTicketRouter);
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//   res.render('tickets/index');
+// });
+app.get('/', async (req, res) => {
+  const Tickets = await Ticket.find();
   res.render('tickets/index');
 });
-
-app.get('/mainpage', (req, res) => {
-  Ticket.find({}, function (err, tickets) {
-    res.render('tickets/mainpage', {
-      ticketsList: tickets,
-    });
-  });
-});
-
 app.listen('3000', (req, res) => {
   console.log(`Server is running at port 3000`);
 });
