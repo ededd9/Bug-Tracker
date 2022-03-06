fd441a60fae237c72421479c32f0d421ae2322f1
Edward Hurtado <edhur9@gmail.com>
2022-03-05 22:54:07 -0500

    no change

--- routes/addTicketRouter.js
+++ routes/addTicketRouter.js
@@ -1,22 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const Ticket = require('../models/tickets');
 router.get('/', (req, res) => {
   res.render('tickets/addTicketScreen');
 });
-router.post('/', async function (req, res) {
-  let newTicket = new Ticket({
-    department: req.body.department,
-    subject: req.body.subject,
-    severity: req.body.severity,
-    description: req.body.description,
-  });
-  try {
-    newTicket.save();
-    res.redirect('/');
-  } catch (e) {
-    console.log(e);
-  }
-});
+
 
 module.exports = router;
