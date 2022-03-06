25d855c61b24f156fafba5e9a9ca68e8978360c4
Edward Hurtado <edhur9@gmail.com>
2022-03-05 22:39:22 -0500

    added mainpage with render tickets functionality

--- routes/mainpage.js
+++ routes/mainpage.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const Ticket = require('../models/tickets');
 const router = express.Router();
-// const Ticket = require('./models/tickets')
 
-router.get('/', (req,res) =>{
-    res.render('tickets/mainpage')
-})
+
+//render mainpage with each ticket from mongoDb
+router.get('/', (req, res) => {
+  Ticket.find({}, function (err, tickets) {
+    res.render('tickets/mainpage', {
+      tickets,
+    });
+  });
+});
+//create new ticket function
+router.post('/', async function (req, res) {
+  //defining Tickets properties
+  let newTicket = new Ticket({
+    department: req.body.department,
+    subject: req.body.subject,
+    severity: req.body.severity,
+    description: req.body.description,
+  });
+  try {
+    Ticket = await newTicket.save();
+    res.render('tickets/mainpage');
+  } catch (e) {
+    console.log(e);
+    res.redirect('/mainpage');
+  }
+});
 
 module.exports = router;
